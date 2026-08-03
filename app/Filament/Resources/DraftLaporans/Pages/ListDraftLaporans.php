484c0b47e3103098735bbe29625d8de8803e0d54
sayaakanlawann <lnxnoa@gmail.com>
2026-08-03 08:38:40 +0800

    create action deleted on draft

--- app/Filament/Resources/DraftLaporans/Pages/ListDraftLaporans.php
+++ app/Filament/Resources/DraftLaporans/Pages/ListDraftLaporans.php
@@ -13,7 +13,7 @@ class ListDraftLaporans extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            // CreateAction::make(),
         ];
     }
 }
